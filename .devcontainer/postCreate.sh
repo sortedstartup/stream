9c17b73a45d769e928a3aa1be0767ab124b84456
xask00 <xask.linus@gmail.com>
2024-11-13 14:53:24 +0000

    init

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+ROOT_DIR=$(pwd)
+echo "Root dir = $ROOT_DIR"
+
+# function to download go dependencies
+download_go_dependencies() {
+    echo "Downloading go dependencies"
+    cd $ROOT_DIR/backend
+    # # GOMAXPROCS is supposed to download all dependencies in parallel, need to verify
+    # GOMAXPROCS=10 go mod download -x
+
+    # This does a build therefore populated build cache and downloads dependencies
+    # Due to this the go run will be extremely fast
+    GOMAXPROCS=10 go mod download -x
+    if [ $? -ne 0 ]; then
+        echo "Ignoring Failure: Failed to download go dependencies"
+        return 0
+    fi
+}
+
+# function to download node dependencies
+download_node_dependencies() {
+    echo "Downloading node dependencies"
+    cd $ROOT_DIR/frontend/ionic
+    npm install
+    if [ $? -ne 0 ]; then
+        echo "Ignoring Failure: Failed to download node dependencies"
+        return 0
+    fi
+}
+
+get_firebase_credentials() {
+    # The env variables $FIREBASE_ADMIN_CREDENTIALS_BASE64 supplied by Github codespaces secret automatically to any codespace
+    if [ -z "$FIREBASE_ADMIN_CREDENTIALS_BASE64" ]; then
+    echo "ERROR: FIREBASE_ADMIN_CREDENTIALS_BASE64 is not set"
+    return 1
+    fi
+
+    echo $FIREBASE_ADMIN_CREDENTIALS_BASE64 | base64 -d > .secrets/firebase-admin-credentials.json
+    if [ $? -ne 0 ]; then
+    echo "ERROR: Failed to decode FIREBASE_ADMIN_CREDENTIALS_BASE64 or write it to file [./firebase-admin-credentials.json]"
+    return 1
+    fi
+
+    echo "Successfully decoded FIREBASE_ADMIN_CREDENTIALS_BASE64 to [./firebase-admin-credentials.json]"
+    return 0
+}
+
+# --- Script starts here ---
+get_firebase_credentials
+download_go_dependencies &
+download_node_dependencies &
+# --- Script ends here ---
+
+# Wait for background function to end
+wait
+
+echo "Script execution ends"
+
---
+++ README.md
@@ -0,0 +1,18 @@
+SortedStartup Stream
+====================
+
+# The Idea
+We wanted a self hosted / cloud hosted open source tool to help us do screen recording, create a library of those recording and share it internally with out team for sharing knowledge, communications, training etc.
+We found few tools which we liked like Loom, but they were closed source and paid per user even if we did not use it much. It felt we were held hostage to their monthly price. So we decided to build our own tool which is open source and can be self hosted.
+
+We noticed that the web APIs available in every browser for recording and playing video make our effort very easy.
+
+# The Tech Stack
+- Frontend: React
+- Backend: golang
+- Database: sqlite
+- Video Store : File System, Sqlite, S3 compatible storage
+
+# License
+As of now we feel GPL v2 is the right license for us.
+We will review it based on community feedback as we go along.
