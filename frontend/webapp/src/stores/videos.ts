ef53523578c8ba7be3d26c6a0118cf467990d6fa
xask00 <xask.linus@gmail.com>
2025-01-21 14:45:36 +0530

    implement api.getVideo()

--- frontend/webapp/src/stores/videos.ts
+++ frontend/webapp/src/stores/videos.ts
@@ -5,7 +5,7 @@
 import { atom, onMount } from "nanostores"
 import { UnaryInterceptor } from "grpc-web";
 import { $authToken } from "../auth/store/auth";
-import { ListVideosRequest, Video, VideoServiceClient } from "../proto/videoservice"
+import { GetVideoRequest, ListVideosRequest, Video, VideoServiceClient } from "../proto/videoservice"
 
 
 export const $videos = atom<Video[]>([])
@@ -38,6 +38,13 @@ export const fetchVideos = async () => {
         pageSize: 10,
     }),{})
 
-
     $videos.set(response.videos)
+}
+
+export const fetchVideo = async (id: string) => {
+    const response = await videoService.GetVideo(GetVideoRequest.fromObject({
+         video_id: id
+    }),{})
+
+    return response
 }
