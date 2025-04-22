9dced979f642d89e1715d7348070801bdc734ad3
Swapnil Srivastava <142904704+Swapnilden@users.noreply.github.com>
2025-04-22 06:44:40 +0530

    Create api implementation for content service (#30)
    * I've added the necessary gRPC methods and corresponding SQL queries for creating comments, including replies
    
    * added firebase config in env file
    
    * This will setup ui changes for comment section with replies.
    
    * writtwn test cases for comment service api
    
    * for commit conflict
    
    * Resolved merge conflicts
    
    * Revert "Resolved merge conflicts"
    
    This reverts commit 05f70258ca30accda4fec4dd8524aa8936b011c0.
    
    * structuring nested replies with comments
    
    * This will solve nested reply issue
    
    * for sending proper api response for nesting of replies with their parent comment
    
    * added firebase config in env file
    
    * writtwn test cases for comment service api
    
    * for commit conflict
    
    * Resolved merge conflicts
    
    * Revert "Resolved merge conflicts"
    
    This reverts commit 05f70258ca30accda4fec4dd8524aa8936b011c0.
    
    * for sending proper api response for nesting of replies with their parent comment
    
    * This will setup ui changes for comment section with replies.
    
    * to seperate the api and queries change from ui branch
    
    * whitespace diff
    
    * Added indexes to improve query performance
    
    * Displaying Username Instead of Random String (#37)
    
    * This will now display the username for both comments and replies instead of random string.
    
    * This will now display the username for both comments and replies instead of UserID
    
    ---------
    
    Co-authored-by: xask00 <xask.linus@gmail.com>
    Co-authored-by: Ram <49852434+janakiramg@users.noreply.github.com>

--- backend/commentservice/db/migrations/1_init.up.sql
+++ backend/commentservice/db/migrations/1_init.up.sql
@@ -1,3 +1,13 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE users (
+    id TEXT PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
 CREATE TABLE comments (
     id TEXT PRIMARY KEY,
     content TEXT NOT NULL,
@@ -5,8 +15,13 @@ CREATE TABLE comments (
     user_id TEXT NOT NULL,
     parent_comment_id TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
+
+CREATE INDEX idx_video_id ON comments(video_id);
+CREATE INDEX idx_user_id ON comments(user_id);
+CREATE INDEX idx_parent_comment_id ON comments(parent_comment_id);
 
 CREATE TABLE comment_likes (
     id TEXT PRIMARY KEY,
@@ -14,5 +29,3 @@ CREATE TABLE comment_likes (
     comment_id TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
-
