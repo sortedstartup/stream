d0fff9f892ecbb04eea8a68c9d326d102a3387d8
ram <janakiram.gollapudi@gmail.com>
2025-06-22 16:50:32 +0000

    Updated user api.go with getUserIfNotExists method

--- backend/userservice/api/api.go
+++ backend/userservice/api/api.go
@@ -1,11 +1,14 @@
 package api
 
 import (
+	"context"
 	"database/sql"
 	"log/slog"
-	"net/http"
+	"time"
 
-	"sortedstartup.com/stream/common/auth"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"sortedstartup.com/stream/common/interceptors"
 	"sortedstartup.com/stream/userservice/config"
 	"sortedstartup.com/stream/userservice/db"
@@ -20,23 +23,8 @@ type UserAPI struct {
 	proto.UnimplementedUserServiceServer
 }
 
-func (s *UserAPI) init() error {
-	s.log.Info("Migrating database", "dbDriver", s.config.DB.Driver, "dbURL", s.config.DB.Url)
-	err := db.MigrateDB(s.config.DB.Driver, s.config.DB.Url)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
 func NewUserAPI(config config.UserServiceConfig) (*UserAPI, error) {
 	slog.Info("NewUserAPI")
-
-	fbAuth, err := auth.NewFirebase()
-	if err != nil {
-		return nil, err
-	}
 
 	childLogger := slog.With("service", "UserAPI")
 
@@ -47,16 +35,67 @@ func NewUserAPI(config config.UserServiceConfig) (*UserAPI, error) {
 
 	dbQueries := db.New(_db)
 
-	ServerMux := http.NewServeMux()
-
 	userAPI := &UserAPI{
 		config:    config,
 		db:        _db,
 		log:       childLogger,
 		dbQueries: dbQueries,
 	}
-
-	ServerMux.Handle("/ensureUser", interceptors.FirebaseHTTPHeaderAuthMiddleware(fbAuth, http.HandlerFunc(userAPI.CreateUserIfNotExistsHandler)))
-
 	return userAPI, nil
+}
+
+func (s *UserAPI) Start() error {
+	return nil
+}
+
+func (s *UserAPI) init() error {
+	s.log.Info("Migrating database", "dbDriver", s.config.DB.Driver, "dbURL", s.config.DB.Url)
+	err := db.MigrateDB(s.config.DB.Driver, s.config.DB.Url)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *UserAPI) CreateUserIfNotExistsHandler(ctx context.Context, req *proto.GetUserByEmailRequest) (*proto.User, error) {
+	s.log.Info("CreateUserIfNotExistsHandler")
+	authContext, err := interceptors.AuthFromContext(ctx)
+	if err != nil {
+		return nil, status.Error(codes.Unauthenticated, "unauthenticated")
+	}
+
+	userEmail := authContext.User.Email
+
+	dbUser, err := s.dbQueries.GetUserByEmail(ctx, userEmail)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// User doesn't exist, create them
+			createParams := db.CreateUserParams{
+				ID:        authContext.User.ID,
+				Username:  authContext.User.Email,
+				Email:     authContext.User.Email,
+				AvatarUrl: "https://example.com/avatar.png",
+				CreatedAt: time.Now(),
+			}
+
+			dbUser, err = s.dbQueries.CreateUser(ctx, createParams)
+			if err != nil {
+				return nil, status.Error(codes.Internal, "failed to create user")
+			}
+		} else {
+			return nil, status.Error(codes.Internal, "internal server error")
+		}
+	}
+
+	// Convert db.User to proto.User
+	protoUser := &proto.User{
+		Id:        dbUser.ID,
+		Username:  dbUser.Username,
+		Email:     dbUser.Email,
+		AvatarUrl: dbUser.AvatarUrl,
+		CreatedAt: timestamppb.New(dbUser.CreatedAt),
+	}
+
+	return protoUser, nil
 }
